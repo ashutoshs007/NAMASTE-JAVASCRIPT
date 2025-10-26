2c55c8fc852c1cdcd4f0287cc24d0bbd7f46e41a
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-10-27 00:23:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+ /*Hoisting is JS*/
+ 
+ 
+//  getName();/**this will print"Namaste JS" */
+//  console.log(x);/*this will print undefined*/
+//  console.log(getName);/*this will print the exact function again*/
+
+ 
+ 
+ var x=8;
+
+
+ 
+
+function getName(){
+    console.log("Namaste JavaScript");
+}
+
+
+
+// console.log(getName);
+ getName();/*Function invocation*/
+ console.log(x);
+
+
+
+
