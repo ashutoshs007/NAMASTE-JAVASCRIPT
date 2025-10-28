f412d6e49c6977a47c180d056a8bfc8da9f70561
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-10-28 22:21:59 +0530

    letandvar

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+console.log("ashutosh");
 let a=10;
-console.log(a);
-var b =100;
- 
+
+
+// let a=100;
+ var a=100;
+
+ //we cannot redecalre the same name in the same scope,not even a single line will run in this case
+
+ //causes SyntaxE; Identifier 'a' has already been declared
+
+
+//  var b=1000;
+
+//  var b =10000;
+
+ //it is posssible in var but not in let
+
+
