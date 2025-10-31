8a65a0b672a60ab62c20a6c29f72f89aa6d3a721
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-10-31 16:09:29 +0530

    shadowing

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-const c=100;
-
-function x(){
-    const c=30;
-    console.log(c);
-}
-x();
-console.log(c);
-
-//even in the function scope ,the shadowing behaves in a similar way.
-
-
-//given  code below is example of illegal shadowing
-let a=20;
+const a =20;
 {
-    var a=20;
-}
-
-//but below one is correct
-
-var a=20;
-{
-    let a=20;
-}
-
-//given belwo is not ileegal shadowing 
-let a=20;
-function x (){
-    var a=20;
+    const a=100;
+    {
+        const a=200;
+        console.log(a);
+        
+    }
 }
