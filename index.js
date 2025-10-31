afbc6185679564956a09d5a0228d16c11b207470
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-10-31 15:12:03 +0530

    shadowing of var variable

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-//block,scope and shadowing
- //Block is also known as Compound Statement
-// if(true){
-//  var a=10;
-//  console.log(a);
-// }
+var a=100;
 
 {
-    var a =10;
+    var a=10;
     let b=20;
     const c=30;
-    // console.log(a);
-    // console.log(b);
-    // console.log(c);
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
-    // console.log(a);
-    // console.log(b);
-    // console.log(c);
+console.log(a);
 
-
-
-
-
-
-
-
-
+//avar a=10 changed value of var a=100, value got changed  both in block scope and global scope 
+// and now both refers to same value .
