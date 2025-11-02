81e06311b963fcee4ed1f961bfdd48e3f897a18e
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-11-02 23:12:56 +0530

    closureproperties

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const a =20;
-{
-    const a=100;
-    {
-        const a=200;
-        console.log(a);
-        
-    }
+//function alongwith its lexical scope forms a closure
+
+
+function x(){
+    // var a =8;
+
+    // you can assign a function to a variable
+
+
+    // var a =function y(){
+    //     console.log(a);
+    // };
+
+
+    
+    // function y(){
+    //     console.log(a);
+    // }
+
+
+
+
+ y();
 }
+x();
+
+
+
+
+//you can pass a function as a parameter as well
+
+// x( function y(){
+//         console.log(a);
+//     });
